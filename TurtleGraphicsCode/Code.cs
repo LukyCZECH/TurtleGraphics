60c468360804ea00e9d2a8dff1ad5427df5c8907
Lukáš Volek <lukas.volek@arbesova.local>
2019-10-24 17:00:03 +0200

    těpic, mám na hlavě nový tsunami -> Natural Disaster 24.10.2019

--- TurtleGraphicsCode/Code.cs
+++ TurtleGraphicsCode/Code.cs
@@ -1,34 +1,56 @@
 ﻿using System;
 
-namespace TurtleGraphicsCode {
+namespace TurtleGraphicsCode
+{
 
-	public class Code {
+    public class Code
+    {
 
-		/// <summary>
-		/// This is the place to put your turtle code
-		/// </summary>
-		public Turtle ToExecute() {
-			Turtle t = new Turtle();
-			t.Rotate(-90);
+        /// <summary>
+        /// This is the place to put your turtle code
+        /// </summary>
+        public Turtle ToExecute()
+        {
+            Turtle t = new Turtle();
+            t.Rotate(-90);
+            for (int i = 0; i < 6; i++) {
+                t.StoreTurtlePosition();
+                for (int e = 0; e < 20; e++) { }
+                {
+                    DrawSection(t, 100);
 
-			for (int i = 0; i < 20; i++) {
-				DrawSection(t, 100);
-			}
-			return t;
-		}
 
-		void DrawSection(Turtle t, int p) {
-			t.Forward(10);
-			t.StoreTurtlePosition();
-			t.Rotate(-45);
-			t.Forward(p);
-			t.RestoreTurtlePosition();
-			t.Rotate(45);
-			t.Forward(p);
-			t.RestoreTurtlePosition(true);
-		}
-	}
+
+                }
+                t.RestoreTurtlePosition();
+                t.Rotate(60);
+            }
+
+
+            t.StoreTurtlePosition();
+
+
+           
+
+            return t;
+
+        }
+
+        void DrawSection(Turtle t, int p)
+        {
+            t.Forward(10);
+            t.StoreTurtlePosition();
+            t.Rotate(-45);
+            t.Forward(p);
+            t.RestoreTurtlePosition();
+            t.Rotate(45);
+            t.Forward(p);
+            t.RestoreTurtlePosition(true);
+
+        }
+    }
 }
+
 
 
 
